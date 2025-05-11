907c8b5c59d7888fac07cd81ea1ec0ca20a6c611
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2025-05-11 17:51:31 +1000

    asd

--- keyboards/handwired/latios_latias/config.h
+++ keyboards/handwired/latios_latias/config.h
@@ -4,7 +4,7 @@
 
 /* Keyboard matrix assignments */
 #define MATRIX_ROW_PINS { GP29, GP28, GP27, GP26, GP22, GP21 }
-#define MATRIX_COL_PINS { GP2, GP3, GP4, GP5, GP6, GP7, GP8 }
+#define MATRIX_COL_PINS { GP8, GP7, GP6, GP5, GP4, GP3, GP2 }
 
 /* Reset */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
