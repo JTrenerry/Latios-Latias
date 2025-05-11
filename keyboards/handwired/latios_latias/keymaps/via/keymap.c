0538e0ee7e0dba2828651679f4c0a995b9cd34ed
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2025-05-11 17:55:34 +1000

    asd

--- keyboards/handwired/latios_latias/keymaps/via/keymap.c
+++ keyboards/handwired/latios_latias/keymaps/via/keymap.c
@@ -8,7 +8,7 @@ void keyboard_post_init_user(void) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
-                                           KC_2,    KC_3,    KC_4,    KC_5,
+                                           KC_2,    KC_3,    KC_2,    KC_5,
                           KC_ESC,  KC_1,   KC_W,    KC_E,    KC_R,    KC_T,
                           KC_TAB,  KC_Q,   KC_S,    KC_D,    KC_F,    KC_G,
                           KC_CAPS, KC_A,   KC_X,    KC_C,    KC_V,    KC_B,
