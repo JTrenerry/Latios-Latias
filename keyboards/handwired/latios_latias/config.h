53998bfe038a23af0c43a5e5124074e5fa8d9023
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2025-05-11 15:19:15 +1000

    asd

---
+++ keyboards/handwired/latios_latias/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define EE_HANDS // Store which side I am in EEPROM
+
+/* Keyboard matrix assignments */
+#define MATRIX_ROW_PINS { GP29, GP28, GP27, GP26, GP22, GP21 }
+#define MATRIX_COL_PINS { GP2, GP3, GP4, GP5, GP6, GP7, GP8 }
+
+/* Reset */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+// This LED blinks when entering bootloader
---
+++ keyboards/handwired/latios_latias/rules.mk
@@ -0,0 +1,9 @@
+# Build Options
+#   change yes to no to disable
+
+AUDIO_SUPPORTED = no
+BOOTMAGIC_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = vendor
+VIA_ENABLE = yes
